9d10a6c48d9b14d25fe655c331471705aadd411a
Adrien BOULAY <aboulay950@gmail.com>
2019-06-16 16:01:26 +0200

    Add webapp templated content

--- devices/webapp/server.py
+++ devices/webapp/server.py
@@ -1,20 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, Response, render_template
 
 from controllers.environment import EnvironmentController
+from presenters.environment import EnvironmentPresenter
 
-app = Flask(__name__)
+
+class EndpointAction(object):
+
+    def __init__(self, action):
+        self.action = action
+
+    def __call__(self, *args):
+        answer = self.action()
+        self.response = Response(answer, status=200, headers={})
+        return self.response
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class FlaskAppWrapper(object):
+    app = None
+
+    def __init__(self, name):
+        self.app = Flask(name)
+
+    def run(self):
+        self.app.run(debug=True, host='0.0.0.0')
+
+    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None):
+        self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler))
 
 
 class WebServer():
     def __init__(self, configuration_file, refresh_time=10):
         controller = EnvironmentController(configuration_file)
         self.environments = controller.get_data()
+        self.presenter = EnvironmentPresenter()
         self.refresh_time = refresh_time
+        self.app_server = FlaskAppWrapper(__name__)
+        self.app_server.add_endpoint(endpoint="/",
+                                     endpoint_name="/",
+                                     handler=self.__index)
+
+    def __index(self):
+        environments = self.presenter.presents(self.environments)
+        return render_template('index.html', environments=environments)
 
     def run(self):
-        app.run(debug=True, host='0.0.0.0')
+        self.app_server.run()
